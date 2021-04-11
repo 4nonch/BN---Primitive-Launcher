ae4b343e89c152ef9427c75e5d5f13041f5152a1
4nonch <abudancer@protonmail.ch>
2021-04-11 15:28:05 +0300

    Update Form1.Designer.cs

--- BN - Primitive Launcher/Form1.Designer.cs
+++ BN - Primitive Launcher/Form1.Designer.cs
@@ -103,7 +103,7 @@ namespace BN_Primitive_Launcher
             this.statusStrip1.BackColor = System.Drawing.Color.LightGray;
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 152);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(259, 22);
             this.statusStrip1.SizingGrip = false;
@@ -324,7 +324,7 @@ namespace BN_Primitive_Launcher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 361);
+            this.ClientSize = new System.Drawing.Size(259, 174);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.kenanBox);
